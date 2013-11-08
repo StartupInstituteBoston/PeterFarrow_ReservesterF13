dd8368a827a74e3f565abc3d15e3548bf80516d2
FarrowLS <peterwfarrow@yahoo.com>
2013-11-08 17:30:33 -0500

    Added welcome page and made it the index page for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Reservester::Application.routes.draw do
   resources :mytempdemos
 
+  get "welcome/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
