231e8260e90a6598f08df1c77c588e9f8c83755f
FarrowLS <peterwfarrow@yahoo.com>
2013-11-10 19:06:08 -0500

    Added show and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Reservester::Application.routes.draw do
 
   get "welcome/index"
+  root 'welcome#index'
+
+  resources :mytempdemos
+
+  resources :restaurants
+  # post GET /restaurants/:id(.:format) restaurants#show
+  get 'restaurants/:id(.:format)' => 'restaurants#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -18,8 +23,6 @@ Reservester::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :mytempdemos
-  resources :restaurants
 
   # Example resource route with options:
   #   resources :products do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107203511) do
+ActiveRecord::Schema.define(version: 20131110224857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20131107203511) do
   create_table "mytempdemos", force: true do |t|
     t.string   "name"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "address"
+    t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
